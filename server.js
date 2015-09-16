9976d7adb726c55cdc52ffe1b3e9435ef43729ac
tropicloud <admin@tropicloud.net>
2015-09-16 12:40:57 -0400

    fix json issues

--- server.js
+++ server.js
@@ -4,21 +4,27 @@ var app = express();
 
 app.route('/centauro/*')
   .get(function (req, res) {
-    var data = fs.readFileSync('/app/data/centauro.json', "utf8");
+    var data = fs.readFileSync(__dirname + '/data/centauro.json', "utf8");
     var callback = req.query.callback;
-    res.send(callback + data);
+    var response = callback + '(' + data + ')';
+    res.set('Content-Type', 'application/json');
+    res.send(response);
   })
 
 app.get('/pontofrio/*', function (req, res) {
-  var data = fs.readFileSync('/app/data/pontofrio.json', "utf8");
+  var data = fs.readFileSync(__dirname + '/data/pontofrio.json', "utf8");
   var callback = req.query.callback;
-  res.send(callback + data);
+  var response = callback + '(' + data + ')';
+  res.set('Content-Type', 'application/json');
+  res.send(response);
 });
 
 app.get('/saraiva/*', function (req, res) {
-  var data = fs.readFileSync('/app/data/saraiva.json', "utf8");
+  var data = fs.readFileSync(__dirname + '/data/saraiva.json', "utf8");
   var callback = req.query.callback;
-  res.send(callback + data);
+  var response = callback + '(' + data + ')';
+  res.set('Content-Type', 'application/json');
+  res.send(response);
 });
 
 app.listen(3000)
